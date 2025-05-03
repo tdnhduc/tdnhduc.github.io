baccf8bceab61ff66c5af8414d0cd3370e65c26f
Duc Nguyen <duc.nguyen2@tyme.com>
2025-05-03 07:53:02 +0700

    check tz

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2025-7-20 11:00", "Asia/Jakarta");
+  let targetDate = moment.tz("2025-7-20 11:00", "Asia/Ho_Chi_Minh");
   console.log(targetDate)
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
